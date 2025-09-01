f3a22ce6dd5ac84df88963b511fca7a32c9edfda
E-Nincy <escotonincy10@outlook.es>
2025-09-01 07:22:42 +0200

    add models & admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Language, Genre
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+admin.site.register(Genre)
