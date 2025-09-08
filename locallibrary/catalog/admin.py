95df178db724402d0fc23f1d7d27e5e7ac7ea014
E-Nincy <escotonincy10@outlook.es>
2025-09-08 07:30:00 +0200

    add code

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Language, Genre
 
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+#-----Register your models here.-----
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# Register the Admin classes for Book using the decorator
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 1
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'language', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Language)
 admin.site.register(Genre)
